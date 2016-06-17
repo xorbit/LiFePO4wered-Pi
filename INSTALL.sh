cd2584224a9c43eaf58a3d1156580635548483fb
Patrick Van Oosterwijck <patrick@silicognition.com>
2016-06-17 15:18:53 -0600

    Add enable UART to config.txt in install script
    The Pi 3 doesn't seem to automatically enable the UART like the
    other Pi's.  This makes shutdowns shut off quicker than expected
    and makes reboots fail (the Pi is shut off during boot).
    
    Adding enable_uart=1 to /boot/config.txt restores this functionality.

--- INSTALL.sh
+++ INSTALL.sh
@@ -35,10 +35,10 @@ then
 fi
 
 # Check whether I2C is enabled in the device tree
-if ! grep -q i2c_arm=on /boot/config.txt
+if ! grep -q ^dtparam=i2c_arm=on /boot/config.txt
 then
   echo "" >> /boot/config.txt
-  echo "# Device tree settings" >> /boot/config.txt
+  echo "# I2C device tree settings" >> /boot/config.txt
   echo "dtparam=i2c1=on" >> /boot/config.txt
   echo "dtparam=i2c_arm=on" >> /boot/config.txt
   echo "Enabling I2C in device tree configuration"
@@ -52,5 +52,16 @@ then
   echo "# Load I2C device module" >> /etc/modules
   echo "i2c-dev" >> /etc/modules
   echo "Enabling I2C device module"
+  echo "You need to reboot for this to take effect!"
+fi
+
+# Check whether the UART is enabled in the device tree
+# We need the UART to detect when the Pi shut down
+if ! grep -q ^enable_uart=1 /boot/config.txt
+then
+  echo "" >> /boot/config.txt
+  echo "# UART device tree settings" >> /boot/config.txt
+  echo "enable_uart=1" >> /boot/config.txt
+  echo "Enabling UART in device tree configuration"
   echo "You need to reboot for this to take effect!"
 fi
