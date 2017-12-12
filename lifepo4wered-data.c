8489cce44ab303ccf7425911ac060836ac7e70d2
Patrick Van Oosterwijck <patrick@silicognition.com>
2017-12-12 10:44:34 -0700

    Fix matching read count and add variable delay
    There was a off-by-one bug in the logic for how many matching
    reads are needed before the data is accepted so that was fixed.
    Also added a variable delay between reads, faster at first, then
    with more delay.
    Added a sign_extend flag to the variable table, which we use to
    flag 16-bit variables we want sign-extended.

--- lifepo4wered-data.c
+++ lifepo4wered-data.c
@@ -42,12 +42,13 @@ struct sVarDef {
   uint8_t       reg[I2C_REG_VER_COUNT];
   uint8_t       read_bytes;
   uint8_t       write_bytes;
+  uint8_t       sign_extend;
 };
 
 /* This table defines scaling for all I2C registers, for different
  * scaling variants */
 
-static const struct sVarScale var_scale[][3] = {
+static const struct sVarScale var_scale[LFP_VAR_COUNT][3] = {
   /* I2C_REG_VER      */  { {      1,      1 }, {      1,      1 }, {      1,      1 } },
   /* I2C_ADDRESS      */  { {      1,      1 }, {      1,      1 }, {      1,      1 } },
   /* LED_STATE        */  { {      1,      1 }, {      1,      1 }, {      1,      1 } },
@@ -93,41 +94,41 @@ static const uint8_t var_scale_variant[I2C_REG_VER_COUNT] = {
 
 /* This table covers definitions of all I2C registers */
 
-static const struct sVarDef var_table[] = {
-  /* I2C_REG_VER      */  { { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 },  1,  0 },
-  /* I2C_ADDRESS      */  { { 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01 },  1,  1 },
-  /* LED_STATE        */  { { 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02 },  1,  1 },
-  /* TOUCH_STATE      */  { { 0x19, 0x1B, 0x1D, 0x23, 0x1F, 0x25, 0x37 },  1,  0 },
-  /* TOUCH_CAP_CYCLES */  { { 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03 },  1,  1 },
-  /* TOUCH_THRESHOLD  */  { { 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04 },  1,  1 },
-  /* TOUCH_HYSTERESIS */  { { 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05 },  1,  1 },
-  /* DCO_RSEL         */  { { 0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06 },  1,  1 },
-  /* DCO_DCOMOD       */  { { 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07 },  1,  1 },
-  /* VIN              */  { { R_NA, R_NA, R_NA, 0x21, R_NA, 0x23, 0x33 },  2,  0 },
-  /* VBAT             */  { { 0x15, 0x17, 0x19, 0x1D, 0x1B, 0x1F, 0x2F },  2,  0 },
-  /* VOUT             */  { { 0x17, 0x19, 0x1B, 0x1F, 0x1D, 0x21, 0x31 },  2,  0 },
-  /* IOUT             */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x35 },  2,  0 },
-  /* VBAT_MIN         */  { { 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08 },  2,  2 },
-  /* VBAT_SHDN        */  { { 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A },  2,  2 },
-  /* VBAT_BOOT        */  { { 0x0C, 0x0C, 0x0C, 0x0C, 0x0C, 0x0C, 0x0C },  2,  2 },
-  /* VOUT_MAX         */  { { 0x0E, 0x0E, 0x0E, 0x0E, 0x0E, 0x0E, 0x0E },  2,  2 },
-  /* VIN_THRESHOLD    */  { { R_NA, R_NA, R_NA, 0x10, R_NA, 0x10, 0x10 },  2,  2 },
-  /* IOUT_SHDN_THRESH */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x1A },  2,  2 },
-  /* VOFFSET_ADC      */  { { R_NA, R_NA, 0x10, 0x12, 0x10, 0x12, R_NA },  2,  2 },
-  /* VBAT_OFFSET      */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x12 },  2,  2 },
-  /* VOUT_OFFSET      */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x14 },  2,  2 },
-  /* VIN_OFFSET       */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x16 },  2,  2 },
-  /* IOUT_OFFSET      */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x18 },  2,  2 },
-  /* AUTO_BOOT        */  { { 0x10, 0x12, 0x14, 0x18, 0x14, 0x18, 0x20 },  1,  1 },
-  /* WAKE_TIME        */  { { 0x12, 0x14, 0x16, 0x1A, 0x18, 0x1C, 0x24 },  2,  2 },
-  /* SHDN_DELAY       */  { { R_NA, 0x10, 0x12, 0x14, 0x12, 0x14, 0x1C },  2,  2 },
-  /* AUTO_SHDN_TIME   */  { { R_NA, R_NA, R_NA, 0x16, R_NA, 0x16, 0x1E },  2,  2 },
-  /* PI_BOOT_TO       */  { { R_NA, R_NA, R_NA, R_NA, 0x15, 0x19, 0x21 },  1,  1 },
-  /* PI_SHDN_TO       */  { { R_NA, R_NA, R_NA, R_NA, 0x16, 0x1A, 0x22 },  1,  1 },
-  /* RTC_TIME         */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x26 },  4,  4 },
-  /* RTC_WAKE_TIME    */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x2A },  4,  4 },
-  /* PI_RUNNING       */  { { 0x14, 0x16, 0x18, 0x1C, 0x1A, 0x1E, 0x2E },  1,  1 },
-  /* CFG_WRITE        */  { { 0x11, 0x13, 0x15, 0x19, 0x17, 0x1B, 0x23 },  1,  1 },
+static const struct sVarDef var_table[LFP_VAR_COUNT] = {
+  /* I2C_REG_VER      */  { { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }, 1, 0, 0 },
+  /* I2C_ADDRESS      */  { { 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01 }, 1, 1, 0 },
+  /* LED_STATE        */  { { 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02 }, 1, 1, 0 },
+  /* TOUCH_STATE      */  { { 0x19, 0x1B, 0x1D, 0x23, 0x1F, 0x25, 0x37 }, 1, 0, 0 },
+  /* TOUCH_CAP_CYCLES */  { { 0x03, 0x03, 0x03, 0x03, 0x03, 0x03, 0x03 }, 1, 1, 0 },
+  /* TOUCH_THRESHOLD  */  { { 0x04, 0x04, 0x04, 0x04, 0x04, 0x04, 0x04 }, 1, 1, 0 },
+  /* TOUCH_HYSTERESIS */  { { 0x05, 0x05, 0x05, 0x05, 0x05, 0x05, 0x05 }, 1, 1, 0 },
+  /* DCO_RSEL         */  { { 0x06, 0x06, 0x06, 0x06, 0x06, 0x06, 0x06 }, 1, 1, 0 },
+  /* DCO_DCOMOD       */  { { 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07 }, 1, 1, 0 },
+  /* VIN              */  { { R_NA, R_NA, R_NA, 0x21, R_NA, 0x23, 0x33 }, 2, 0, 1 },
+  /* VBAT             */  { { 0x15, 0x17, 0x19, 0x1D, 0x1B, 0x1F, 0x2F }, 2, 0, 1 },
+  /* VOUT             */  { { 0x17, 0x19, 0x1B, 0x1F, 0x1D, 0x21, 0x31 }, 2, 0, 1 },
+  /* IOUT             */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x35 }, 2, 0, 1 },
+  /* VBAT_MIN         */  { { 0x08, 0x08, 0x08, 0x08, 0x08, 0x08, 0x08 }, 2, 2, 1 },
+  /* VBAT_SHDN        */  { { 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A, 0x0A }, 2, 2, 1 },
+  /* VBAT_BOOT        */  { { 0x0C, 0x0C, 0x0C, 0x0C, 0x0C, 0x0C, 0x0C }, 2, 2, 1 },
+  /* VOUT_MAX         */  { { 0x0E, 0x0E, 0x0E, 0x0E, 0x0E, 0x0E, 0x0E }, 2, 2, 1 },
+  /* VIN_THRESHOLD    */  { { R_NA, R_NA, R_NA, 0x10, R_NA, 0x10, 0x10 }, 2, 2, 1 },
+  /* IOUT_SHDN_THRESH */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x1A }, 2, 2, 1 },
+  /* VOFFSET_ADC      */  { { R_NA, R_NA, 0x10, 0x12, 0x10, 0x12, R_NA }, 2, 2, 1 },
+  /* VBAT_OFFSET      */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x12 }, 2, 2, 1 },
+  /* VOUT_OFFSET      */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x14 }, 2, 2, 1 },
+  /* VIN_OFFSET       */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x16 }, 2, 2, 1 },
+  /* IOUT_OFFSET      */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x18 }, 2, 2, 1 },
+  /* AUTO_BOOT        */  { { 0x10, 0x12, 0x14, 0x18, 0x14, 0x18, 0x20 }, 1, 1, 0 },
+  /* WAKE_TIME        */  { { 0x12, 0x14, 0x16, 0x1A, 0x18, 0x1C, 0x24 }, 2, 2, 0 },
+  /* SHDN_DELAY       */  { { R_NA, 0x10, 0x12, 0x14, 0x12, 0x14, 0x1C }, 2, 2, 0 },
+  /* AUTO_SHDN_TIME   */  { { R_NA, R_NA, R_NA, 0x16, R_NA, 0x16, 0x1E }, 2, 2, 0 },
+  /* PI_BOOT_TO       */  { { R_NA, R_NA, R_NA, R_NA, 0x15, 0x19, 0x21 }, 1, 1, 0 },
+  /* PI_SHDN_TO       */  { { R_NA, R_NA, R_NA, R_NA, 0x16, 0x1A, 0x22 }, 1, 1, 0 },
+  /* RTC_TIME         */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x26 }, 4, 4, 0 },
+  /* RTC_WAKE_TIME    */  { { R_NA, R_NA, R_NA, R_NA, R_NA, R_NA, 0x2A }, 4, 4, 0 },
+  /* PI_RUNNING       */  { { 0x14, 0x16, 0x18, 0x1C, 0x1A, 0x1E, 0x2E }, 1, 1, 0 },
+  /* CFG_WRITE        */  { { 0x11, 0x13, 0x15, 0x19, 0x17, 0x1B, 0x23 }, 1, 1, 0 },
 };
 
 /* I2C register version detected */
@@ -140,7 +141,20 @@ static int32_t i2c_reg_ver = 0;
 
 /* I2C identical reads requirement (minimum 2) */
 
-#define I2C_IDENTICAL_READS 2
+#define I2C_IDENTICAL_READS 3
+
+/* I2C retry delay */
+
+static const useconds_t i2c_retry_delay[I2C_RETRIES] = {
+  300,
+  300,
+  300,
+  1000,
+  2000,
+  4000,
+  6000,
+  8000
+};
 
 
 /* Determine if the specified variable can be accessed in the specified
@@ -209,13 +223,15 @@ int32_t read_lifepo4wered(enum eLiFePO4weredVar var) {
     uint8_t reg = var == I2C_REG_VER ?
                   I2C_REG_VER : var_def->reg[i2c_reg_ver - 1];
     uint8_t read_bytes = var == I2C_REG_VER ? 1 : var_def->read_bytes;
-    for (uint8_t retries = I2C_RETRIES; retries; retries--) {
+    uint8_t sign_extend = var == I2C_REG_VER ? 0 : var_def->sign_extend;
+    for (uint8_t retries = 0; retries < I2C_RETRIES; retries++) {
+      usleep(i2c_retry_delay[retries]);
       if (read_lifepo4wered_data(reg, read_bytes, data.b)) {
-        if (data.i == match_data.i) {
-          if (match_tries && match_tries >= I2C_IDENTICAL_READS - 1) {
+        if (!match_tries || data.i == match_data.i) {
+          if (match_tries >= I2C_IDENTICAL_READS - 1) {
             const struct sVarScale *scale =
                     &var_scale[var][var_scale_variant[i2c_reg_ver - 1]];
-	    if (read_bytes == 2) {
+	    if (sign_extend) {
               data.i = data.h[0];
             }
             return (le32toh(data.i) * scale->mul + scale->div / 2)
@@ -227,7 +243,6 @@ int32_t read_lifepo4wered(enum eLiFePO4weredVar var) {
         }
         match_data.i = data.i;
       }
-      usleep(2000);
     }
     return -2;
   }
